56bb37cdad608a196cfe2ba5f2643aaeffd3f43e
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2026-09-17 00:23:47 -0300

    feat(website): style the docs surface with the DurinDoor palette

--- website/src/app/docs/layout.js
+++ website/src/app/docs/layout.js
@@ -2,6 +2,7 @@ import { DocsLayout } from "fumadocs-ui/layouts/docs";
 import { RootProvider } from "fumadocs-ui/provider/next";
 import { source } from "@site/lib/source";
 import { baseOptions } from "@site/lib/layout.shared";
+import "./docs.css";
 
 export default function Layout({ children }) {
   return (
--- website/src/lib/layout.shared.js
+++ website/src/lib/layout.shared.js
@@ -3,7 +3,7 @@ export function baseOptions() {
     nav: {
       title: (
         <>
-          <img src="/durindoor-wordmark.png" alt="" width={26} height={26} />
+          <img src="/durindoor-wordmark.png" alt="DurinDoor" width={26} height={26} />
           DurinDoor
         </>
       ),
